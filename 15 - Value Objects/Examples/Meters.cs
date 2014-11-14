6cfb6c865bf38f971ab93024db07c89fbb4b02f5
nick <nick@ntcoding.co.uk>
2014-11-14 12:01:30 +0000

    replaced height example with meters

---
+++ 15 - Value Objects/Examples/Meters.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    // Value Object
+    public class Meters
+    {
+        public Meters(decimal distanceInMeters)
+        {
+            if (distanceInMeters < (decimal)0.0)
+                throw new DistancesInMetersCannotBeNegative();
+
+            this.DistanceInMeters = distanceInMeters;
+        }
+
+        protected decimal DistanceInMeters { get; private set; }
+
+        public Yards ToYards()
+        {
+            return new Yards(DistanceInMeters * (decimal)1.0936133);
+        }
+
+        public Kilometers ToKilometers()
+        {
+            return new Kilometers(DistanceInMeters / 1000);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var m = obj as Meters;
+            if (m == null) return false;
+
+            return To2Dp(m.DistanceInMeters) == To2Dp(DistanceInMeters);
+        }
+
+        private decimal To2Dp(decimal distanceInMeters)
+        {
+            return Math.Round(distanceInMeters, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+
+    public class Yards
+    {
+        public Yards(decimal distanceInYards)
+        {
+
+        }
+
+        // ...
+    }
+
+    public class Kilometers
+    {
+        public Kilometers(decimal distainceInKillometers)
+        {
+
+        }
+
+        // ...
+    }
+
+    public class DistancesInMetersCannotBeNegative : Exception { }
+
+    namespace MetersWithBaseClass
+    {
+        public class Meters : ValueObject<Meters>
+        {
+            public Meters(decimal distanceInMeters)
+            {
+                if (distanceInMeters < (decimal)0.0)
+                    throw new DistancesInMetersCannotBeNegative();
+
+                this.DistanceInMeters = distanceInMeters;
+            }
+
+            protected decimal DistanceInMeters { get; private set; }
+
+            public Yards ToYards()
+            {
+                return new Yards(DistanceInMeters * (decimal)1.0936133);
+            }
+
+            public Kilometers ToKilometers()
+            {
+                return new Kilometers(DistanceInMeters / 1000);
+            }
+
+            protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+            {
+                return new List<Object> { DistanceInMeters };
+            }
+        }
+    }
+}
---
+++ 15 - Value Objects/Tests/Tests/Meters_equality_tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Examples;
+
+namespace Tests
+{
+    [TestClass]
+    public class Meters_equality_tests
+    {
+        [TestMethod]
+        public void Same_distances_are_equal_even_if_different_references()
+        {
+            var oneMeter = new Meters((decimal)1);
+            var oneMeterX = new Meters((decimal)1);
+            Assert.AreEqual(oneMeter, oneMeterX);
+
+            var fiftyPoint25 = new Meters((decimal)50.25);
+            var fiftyPoint25X = new Meters((decimal)50.25);
+            Assert.AreEqual(fiftyPoint25, fiftyPoint25X);
+        }
+
+        [TestMethod]
+        public void Same_distances_are_equal_even_if_different_references_with_base()
+        {
+            var oneMeter = new Examples.MetersWithBaseClass.Meters((decimal)11.22);
+            var oneMeterX = new Examples.MetersWithBaseClass.Meters((decimal)11.22);
+            Assert.AreEqual(oneMeter, oneMeterX);
+
+            var fiftyMeters = new Examples.MetersWithBaseClass.Meters(50);
+            var fiftyMetersX = new Examples.MetersWithBaseClass.Meters(50);
+            Assert.AreEqual(fiftyMeters, fiftyMetersX);
+        }
+    }
+}
