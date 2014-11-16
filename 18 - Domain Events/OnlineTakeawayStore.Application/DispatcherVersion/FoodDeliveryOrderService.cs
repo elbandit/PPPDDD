fe93f665b3775e6049ade28ecb08347eedb0c84f
Nick <nick@ntcoding.co.uk>
2014-11-16 21:12:25 +0000

    added events dispatcher example

---
+++ 18 - Domain Events/OnlineTakeawayStore.Application/DispatcherVersion/FoodDeliveryOrderService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineTakeawayStore.Domain.DispatcherVersion;
+
+namespace OnlineTakeawayStore.Application.DispatcherVersion
+{
+    public class FoodDeliveryOrderService
+    {
+        private static int Id = 0;
+
+        // sends real-time notifications to browser and restaurant
+        private INotificationChannel clientChannel;
+        private IRestaurantConnector connector;
+        private IEventDispatcher dispatcher;
+
+        public FoodDeliveryOrderService(INotificationChannel clientChannel, IRestaurantConnector connector,
+            IEventDispatcher dispatcher)
+        {
+            this.clientChannel = clientChannel;
+            this.connector = connector;
+            this.dispatcher = dispatcher;
+        }
+
+        public void PlaceFoodDeliveryOrder(PlaceFoodDeliveryOrderRequest request)
+        {
+            var id = Id++; // for demonstration purposes only
+
+            dispatcher.Register<FoodDeliveryOrder>(o =>
+            {
+                clientChannel.Publish("ORDER_ACKNOWLEDGED");
+            });
+
+            var order = new FoodDeliveryOrder(
+                id, request.CustomerId, request.RestaurantId, request.MenuItemIds,
+                request.DeliveryTime
+            );
+
+            foreach (var ev in order.RecordedEvents)
+            {
+                dispatcher.Dispatch(ev);
+            }
+        }
+    }
+}
---
+++ 18 - Domain Events/OnlineTakeawayStore.Domain/DispatcherVersion/FoodDeliveryOrder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineTakeawayStore.Domain.DispatcherVersion.Events;
+
+namespace OnlineTakeawayStore.Domain.DispatcherVersion
+{
+    public class FoodDeliveryOrder
+    {
+        public FoodDeliveryOrder(int id, int customerId, int restuarantId, List<int> menuItemIds,
+            DateTime deliveryTime)
+        {
+            this.Id = id;
+            this.CustomerId = customerId;
+            this.RestaurantId = restuarantId;
+            this.MenuItemIds = menuItemIds;
+            this.RequestedDeliveryTime = deliveryTime;
+            this.RecordedEvents = new List<Object>();
+            Status = FoodDeliveryOrderSteps.Pending;
+
+            RecordedEvents.Add(new FoodDeliveryOrderCreated(this));
+        }
+
+        public int Id { get; private set; }
+
+        public FoodDeliveryOrderSteps Status { get; private set; }
+
+        public int CustomerId { get; private set; }
+
+        public int RestaurantId { get; private set; }
+
+        public List<int> MenuItemIds { get; private set; }
+
+        public DateTime RequestedDeliveryTime { get; private set; }
+
+        public List<Object> RecordedEvents { get; private set; }
+
+        public void Confirm()
+        {
+            Status = FoodDeliveryOrderSteps.Confirmed;
+
+            RecordedEvents.Add(new FoodDeliveryOrderConfirmed(this));
+        }
+
+        public void Invalidate()
+        {
+            Status = FoodDeliveryOrderSteps.Invalidated;
+        }
+    }
+}
