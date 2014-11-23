088749001cbe57ee5da584f36166c302e3e2c9ff
Nick <nick@ntcoding.co.uk>
2014-11-23 19:08:20 +0000

    applied folder conventions to value objects chapter

---
+++ 15 - Value Objects/Examples/Model/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    // Entity
+    public class BankAccount
+    {
+        public BankAccount(Guid id, Money startingBalance)
+        {
+            this.Id = id;
+            this.Balance = startingBalance;
+        }
+
+        public Guid Id { get; private set; }
+
+        public Money Balance { get; private set; }
+
+        // ..
+    }
+
+    // Value Object
+    public class Money
+    {
+        public Money(int amount, Currency currency)
+        {
+            this.Amount = amount;
+            this.Currency = currency;
+        }
+
+        private int Amount { get; set; }
+
+        private Currency Currency { get; set; }
+
+        // ..
+    }
+
+    public enum Currency 
+    {
+        Dollars,
+        Pounds,
+    }
+}
---
+++ 15 - Value Objects/Examples/Model/Customer.cs
@@ -0,0 +1,95 @@
+﻿using Examples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    // Entity
+    public class Customer
+    {
+        public Customer(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+        public IEnumerable<PhoneNumber> PhoneNumbers { get; set; }
+
+        // ..
+    }
+
+    public class PhoneNumber : ValueObject<PhoneNumber>
+    {
+        public readonly string Number;
+
+        public PhoneNumber(string number)
+        {
+            this.Number = number;
+        }
+
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            return new object[] { Number };
+        }
+
+        // ..
+    }
+}
+
+namespace CustomerWithAddressBook
+{
+    // Entity
+    public class Customer
+    {
+        public Customer(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+        public PhoneBook PhoneNumbers { get; set; }
+
+        // ..
+    }
+
+    public class PhoneBook : ValueObject<PhoneBook>
+    {
+        public readonly PhoneNumber HomeNumber;
+        public readonly PhoneNumber MobileNumber;
+        public readonly PhoneNumber WorkNumber;
+
+        public PhoneBook(PhoneNumber homeNum, PhoneNumber mobileNum, PhoneNumber workNum)
+        {
+            this.HomeNumber = homeNum;
+            this.MobileNumber = mobileNum;
+            this.WorkNumber = workNum;
+        }
+
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            return new object[] { HomeNumber, MobileNumber, WorkNumber };
+        }
+    }
+
+    public class PhoneNumber : ValueObject<PhoneNumber>
+    {
+        public readonly string Number;
+
+        public PhoneNumber(string number)
+        {
+            this.Number = number;
+        }
+
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            return new object[] { Number };
+        }
+
+        // ..
+    }
+}
