b641f72567326873c8309d4f0c9b8e54757f4a12
Nick <nick@ntcoding.co.uk>
2014-11-23 19:25:30 +0000

    applied folder convention to domain services chapter

---
+++ 17 - Domain Services/PPPDDDChap18.DomainServices/Model/RomanceOMeter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPDDDChap18.DomainServices
+{
+    // Domain Service - part of Ubiquitous Language
+    public class RomanceOMeter
+    {
+        // stateless - collaborators are allowed, though
+
+        // behavior-only
+        public CompatibilityRating AssessCompatibility(LoveSeeker seeker1, LoveSeeker seeker2)
+        {
+            var rating = new CompatibilityRating();
+
+            // orchestrate Entities:
+            // compare dating history, blood type, lifestyle etc
+            if (seeker1.BloodType == seeker2.BloodType)
+            {
+                rating = rating.Boost(CompatibilityRating(250));
+            }
+
+            // ..
+
+            // return another Domain Object (Value Object in this case)
+            return rating;
+        }
+
+        private CompatibilityRating CompatibilityRating(int value)
+        {
+            // ...
+            return new CompatibilityRating();
+        }
+    }
+
+
+    // entity
+    public class LoveSeeker
+    {
+        public Guid Id { get; protected set; }
+
+        public BloodType BloodType { get; private set; }
+
+        // ...
+    }
+
+    // Value Objects
+
+    public class CompatibilityRating
+    {
+        public CompatibilityRating Boost(CompatibilityRating rating)
+        {
+            // ..
+            return null;
+        }
+
+        // ..
+    }
+
+    public class BloodType
+    {
+        // ...
+    }
+}
