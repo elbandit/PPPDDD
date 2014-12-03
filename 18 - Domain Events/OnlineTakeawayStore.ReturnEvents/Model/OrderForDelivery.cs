1f429f1b1338dfd69e57615b18cec051fe0bc612
nick <nick@ntcoding.co.uk>
2014-12-03 12:29:24 +0000

    added new return events domain events example

---
+++ 18 - Domain Events/OnlineTakeawayStore.ReturnEvents/Model/OrderForDelivery.cs
@@ -0,0 +1,42 @@
+﻿using OnlineTakeawayStore.ReturnEvents.Model.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTakeawayStore.ReturnEvents.Model
+{
+    public class OrderForDelivery
+    {
+        public Guid Id { get; private set; }
+        private DateTime TimeOfOrderBeingPlaced { get; set; }
+        private DateTime TimeThatPizzaWasDelivered { get; set; }
+        private FoodDeliveryOrderSteps Status { get; set; }
+        private IDeliveryGuaranteeOffer DeliveryGuaranteeOffer { get; set; }
+
+        public OrderForDelivery(Guid id, Guid customerId, Guid restuarantId, List<int> menuItemIds, DateTime timeOfOrderBeingPlaced)
+        {
+            Id = id;
+            TimeOfOrderBeingPlaced = timeOfOrderBeingPlaced;
+            Status = FoodDeliveryOrderSteps.Pending;
+            RecordedEvents = new List<Object>();
+        }
+
+        public List<Object> RecordedEvents { get; private set; }
+
+        public void ConfirmReceipt(DateTime timeThatPizzaWasDelivered)
+        {
+            if (Status != FoodDeliveryOrderSteps.Delivered)
+            {
+                TimeThatPizzaWasDelivered = timeThatPizzaWasDelivered;
+                Status = FoodDeliveryOrderSteps.Delivered;
+                if (DeliveryGuaranteeOffer.IsNotSatisfiedBy(TimeOfOrderBeingPlaced, TimeThatPizzaWasDelivered))
+                {
+                     RecordedEvents.Add(new DeliveryGuaranteeFailed(this));
+                }
+            }
+        }
+
+    }
+}
