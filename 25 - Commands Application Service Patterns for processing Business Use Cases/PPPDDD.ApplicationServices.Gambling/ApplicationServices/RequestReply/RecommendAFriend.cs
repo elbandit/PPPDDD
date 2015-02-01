b91c879c0fe2f4655aaba52f8dc6bd678e17e35f
Nick <nick@ntcoding.co.uk>
2015-02-01 09:34:05 +0000

    added app services request reply example

---
+++ 25 - Commands Application Service Patterns for processing Business Use Cases/PPPDDD.ApplicationServices.Gambling/ApplicationServices/RequestReply/RecommendAFriend.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+using NewAccount = Domain.NewAccount;
+using PPPDDD.ApplicationServices.Gambling.ApplicationServices.ErrorHandling;
+
+namespace PPPDDD.ApplicationServices.Gambling.ApplicationServices.RequestReply
+{
+    public class RecommendAFriendService
+    {
+        private IReferralPolicy policy;
+
+        public RecommendAFriendService(Domain.IReferralPolicy policy)
+        {
+            this.policy = policy;
+        }
+
+        public RecommendAFriendResponse RecommendAFriend(RecommendAFriendRequest request)
+        {
+            try
+            {
+                var command = new RecommendAFriend
+                {
+                    ReferrerId = request.ReferrerId,
+                    Friend = request.Friend
+                };
+
+                policy.Apply(command);
+
+                return new RecommendAFriendResponse
+                {
+                    Status = RecommendAFriendStatus.Success
+                };
+            }
+            catch (ReferralRejectedDueToLongTermOutstandingBalance)
+            {
+                return new RecommendAFriendResponse
+                {
+                    Status = RecommendAFriendStatus.ReferralRejected
+                };
+            }
+        }
+    }
+
+    public class RecommendAFriendRequest
+    {
+        public int ReferrerId { get; set; }
+
+        public NewAccount Friend { get; set; }
+    }
+
+    public class RecommendAFriendResponse
+    {
+        public RecommendAFriendStatus Status { get; set; }
+    }
+
+    public enum RecommendAFriendStatus
+    {
+        Success,
+        ReferralRejected
+    }
+}
